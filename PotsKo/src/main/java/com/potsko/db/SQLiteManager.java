35a017c9599629f5ddd94c2a70dfe8a3305a67b7
Renz Viloria <viloriarc@students.national-u.edu.ph>
2025-05-23 03:04:19 +0800

    SQlite connection for the java classes

--- PotsKo/src/main/java/com/potsko/db/SQLiteManager.java
+++ PotsKo/src/main/java/com/potsko/db/SQLiteManager.java
@@ -1,5 +1,14 @@
+// Manages database connections 
 package com.potsko.db;
 
-public class SQLiteManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SQLiteManager {
+    private static final String DB_URL = "jdbc:sqlite:src/main/java/com/potsko/db/potsko.db"; // File path for db
+      
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
 }
