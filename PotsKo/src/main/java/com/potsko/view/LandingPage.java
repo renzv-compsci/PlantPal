2b340024fad27e5e70cb51ea284204a09148187f
Renz Viloria <viloriarc@students.national-u.edu.ph>
2025-05-26 02:26:50 +0800

    Updated Landing Page

--- PotsKo/src/main/java/com/potsko/view/LandingPage.java
+++ PotsKo/src/main/java/com/potsko/view/LandingPage.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Image;
+import java.awt.Panel;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class LandingPage extends JFrame {
     
@@ -28,33 +34,74 @@ public class LandingPage extends JFrame {
         frame.setIconImage(scaledImage);
         frame.setVisible(true);
 
-        // Adding dynamic text for the Landing Page using try catch method
+        // Uses try catch method for debugging purposes (this loads text)
         try (InputStream fontStream = LandingPage.class.getResourceAsStream("/font/AnnapurnaSIL-Bold.ttf")) {
             Font customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(128f);
 
+            // Creating each label for dynamic text layout
             JLabel label = new JLabel ("PotsKo:");
             label.setForeground(new Color(34, 82, 20));
+            label.setBorder(BorderFactory.createEmptyBorder(144, 227, 0, 0));
             label.setFont(customFont);
             frame.add(label);
+
         } catch (FontFormatException | IOException e) {
             System.err.println("Error loading font 1: " + e.getMessage());
         } 
+        frame.pack();
         frame.setVisible(true);
 
+        // Uses try catch method for debugging purposes (this loads text)
         try (InputStream fontStream = LandingPage.class.getResourceAsStream("/font/AnticDidone-Regular.ttf")) {
-            Font customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(90f);
-
-            JLabel label = new JLabel ("A Smart way to GO GREEN");
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+    
+            // Creating each label for dynamic text layout
+            JLabel label = new JLabel ("A Smart way");
             label.setForeground(new Color (30, 30, 30));
-            label.setFont(customFont);
-            frame.add(label);
+            label.setBorder(BorderFactory.createEmptyBorder(279, 227, 0, 0));
+            label.setFont(customFont.deriveFont(90f));
+
+            JLabel label1 = new JLabel ("to GO GREEN");
+            label1.setForeground(new Color (30, 30, 30));
+            label1.setBorder(BorderFactory.createEmptyBorder(0, 227, 376, 0));
+            label1.setFont(customFont.deriveFont(96f));
+
+            // Let stack multiple labels for dynamic layout
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            panel.add(label);
+            panel.add(label1);
+            frame.add(panel);
         } catch (FontFormatException | IOException e) {
             System.err.println("Error loading font 2: " + e.getMessage());
         }
         frame.setVisible(true);
 
-    }
+        // try catch method for debugging purposes (this loads images)
+        try {
+            ImageIcon img1 = new ImageIcon(getClass().getResource("/images/appImg/plant1.png"));
+            ImageIcon img2 = new ImageIcon(getClass().getResource("/images/appImg/plant3.png"));
+            
+            // Setting image size 
+            Image scaledImg1 = img1.getImage().getScaledInstance(649, 649, Image.SCALE_SMOOTH);
+            Image scaledImg2 = img2.getImage().getScaledInstance(695, 695, Image.SCALE_SMOOTH);
 
+            JLabel label1 = new JLabel(new ImageIcon(scaledImg1));
+            JLabel label2 = new JLabel(new ImageIcon(scaledImg2));
+            
+            
+            JPanel panel = new JPanel();
+            panel.add(label1);
+            panel.add(label2);
+
+            frame.add(panel);
+            frame.pack();
+        } catch (Exception e) {
+            System.err.println("Failed to load the image: " + e.getMessage());
+        }
+        frame.setVisible(true);
+
+    }
     public static void main(String[] args) {
         new LandingPage();
     }
