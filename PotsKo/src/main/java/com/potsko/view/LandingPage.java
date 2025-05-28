0eed7ea6b6fb3ffe84a74c9927b3885fd0a53705
Renz Viloria <viloriarc@students.national-u.edu.ph>
2025-05-28 21:17:58 +0800

    Updated Landing Page

--- PotsKo/src/main/java/com/potsko/view/LandingPage.java
+++ PotsKo/src/main/java/com/potsko/view/LandingPage.java
@@ -1,20 +1,20 @@
 package com.potsko.view;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.Image;
-import java.awt.Panel;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 public class LandingPage extends JFrame {
@@ -32,75 +32,78 @@ public class LandingPage extends JFrame {
         ImageIcon icon = new ImageIcon("PotsKo\\src\\main\\resources\\images\\UIicons\\PotsKo_Logo.png");
         Image scaledImage = icon.getImage().getScaledInstance(500, 500, Image.SCALE_SMOOTH);
         frame.setIconImage(scaledImage);
-        frame.setVisible(true);
+
+        // Setting up JLayered Pane for image design positioning (Allows image overlapping)
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(2000,2000));
+        frame.setContentPane(layeredPane);
+
+        // JPanel for the positions of dynamic text 
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+        textPanel.setBounds (250, 220, 2000, 2000);
 
         // Uses try catch method for debugging purposes (this loads text)
         try (InputStream fontStream = LandingPage.class.getResourceAsStream("/font/AnnapurnaSIL-Bold.ttf")) {
             Font customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(128f);
 
             // Creating each label for dynamic text layout
-            JLabel label = new JLabel ("PotsKo:");
-            label.setForeground(new Color(34, 82, 20));
-            label.setBorder(BorderFactory.createEmptyBorder(144, 227, 0, 0));
-            label.setFont(customFont);
-            frame.add(label);
+            JLabel label1 = new JLabel ("PotsKo:");
+            label1.setForeground(new Color(34, 82, 20));
+            label1.setFont(customFont);
+            label1.setAlignmentX(Component.LEFT_ALIGNMENT);
+            textPanel.add(label1);
 
         } catch (FontFormatException | IOException e) {
             System.err.println("Error loading font 1: " + e.getMessage());
         } 
-        frame.pack();
-        frame.setVisible(true);
 
         // Uses try catch method for debugging purposes (this loads text)
         try (InputStream fontStream = LandingPage.class.getResourceAsStream("/font/AnticDidone-Regular.ttf")) {
             Font customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
     
             // Creating each label for dynamic text layout
-            JLabel label = new JLabel ("A Smart way");
-            label.setForeground(new Color (30, 30, 30));
-            label.setBorder(BorderFactory.createEmptyBorder(279, 227, 0, 0));
-            label.setFont(customFont.deriveFont(90f));
+            JLabel label2 = new JLabel ("A Smart way");
+            label2.setBorder(BorderFactory.createEmptyBorder(-20, 0, 0, 0)); // negative bottom border
+            label2.setForeground(new Color (30, 30, 30));
+            label2.setAlignmentX(Component.LEFT_ALIGNMENT);
+            label2.setFont(customFont.deriveFont(90f));
+            textPanel.add(label2);
 
-            JLabel label1 = new JLabel ("to GO GREEN");
-            label1.setForeground(new Color (30, 30, 30));
-            label1.setBorder(BorderFactory.createEmptyBorder(0, 227, 376, 0));
-            label1.setFont(customFont.deriveFont(96f));
-
-            // Let stack multiple labels for dynamic layout
-            JPanel panel = new JPanel();
-            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-            panel.add(label);
-            panel.add(label1);
-            frame.add(panel);
+            JLabel label3 = new JLabel ("to GO GREEN");
+            label3.setBorder(BorderFactory.createEmptyBorder(-10, 0, 0, 0)); // negative top border
+            label3.setForeground(new Color (30, 30, 30));
+            label3.setAlignmentX(Component.LEFT_ALIGNMENT);
+            label3.setFont(customFont.deriveFont(96f));
+            textPanel.add(label3);
+        
         } catch (FontFormatException | IOException e) {
             System.err.println("Error loading font 2: " + e.getMessage());
-        }
-        frame.setVisible(true);
+        }  
 
-        // try catch method for debugging purposes (this loads images)
         try {
-            ImageIcon img1 = new ImageIcon(getClass().getResource("/images/appImg/plant1.png"));
-            ImageIcon img2 = new ImageIcon(getClass().getResource("/images/appImg/plant3.png"));
-            
-            // Setting image size 
-            Image scaledImg1 = img1.getImage().getScaledInstance(649, 649, Image.SCALE_SMOOTH);
-            Image scaledImg2 = img2.getImage().getScaledInstance(695, 695, Image.SCALE_SMOOTH);
+        // Image overlapping layout using layeredPane
+        ImageIcon leftPlant = new ImageIcon(getClass().getResource("/images/appImg/plant3.png"));
+        Image leftPlantScl = leftPlant.getImage().getScaledInstance(695, 695, Image.SCALE_SMOOTH);
+        JLabel leftPlantLbl = new JLabel(new ImageIcon(leftPlantScl));
+        leftPlantLbl.setBounds(-304, -143, 695, 695);
+        layeredPane.add(leftPlantLbl, JLayeredPane.MODAL_LAYER);
 
-            JLabel label1 = new JLabel(new ImageIcon(scaledImg1));
-            JLabel label2 = new JLabel(new ImageIcon(scaledImg2));
-            
-            
-            JPanel panel = new JPanel();
-            panel.add(label1);
-            panel.add(label2);
-
-            frame.add(panel);
-            frame.pack();
+        ImageIcon rightPlant = new ImageIcon(getClass().getResource("/images/appImg/plant1.png"));
+        Image rightPlantScl = rightPlant.getImage().getScaledInstance(649, 649, Image.SCALE_SMOOTH);
+        JLabel rightPlantLbl = new JLabel(new ImageIcon(rightPlantScl));
+        rightPlantLbl.setBounds(1470, 565, 649, 649);
+        layeredPane.add(rightPlantLbl, JLayeredPane.MODAL_LAYER);
         } catch (Exception e) {
             System.err.println("Failed to load the image: " + e.getMessage());
         }
-        frame.setVisible(true);
 
+        layeredPane.add(textPanel, JLayeredPane.PALETTE_LAYER);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
     }
     public static void main(String[] args) {
         new LandingPage();
