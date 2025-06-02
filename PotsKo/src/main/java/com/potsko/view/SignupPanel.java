957245e5f0b7ced6b10ae46cc753c8b5ad2e1f5a
Renz Viloria <viloriarc@students.national-u.edu.ph>
2025-06-03 00:38:53 +0800

    Initial set up for the SignupPanel

--- PotsKo/src/main/java/com/potsko/view/SignupPanel.java
+++ PotsKo/src/main/java/com/potsko/view/SignupPanel.java
@@ -1,9 +1,23 @@
 package com.potsko.view;
 
-import javax.swing.JFrame;
 
-public class SignupPanel extends JFrame{
-    public SignupPanel(MainFrame mainframe) {
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class SignupPanel extends JPanel{
+    public SignupPanel(MainFrame mainFrame) {
+        
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Signup Panel");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> mainFrame.showLanding());
+        add(back, BorderLayout.SOUTH);
     }
 }
