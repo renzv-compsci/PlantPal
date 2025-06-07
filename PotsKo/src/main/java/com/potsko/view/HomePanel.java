a1735db69855252deb987e2dd18af4c15ab6e806
Renz Viloria <viloriarc@students.national-u.edu.ph>
2025-06-07 23:20:22 +0800

    Add place holder for HomePanel layout

--- PotsKo/src/main/java/com/potsko/view/HomePanel.java
+++ PotsKo/src/main/java/com/potsko/view/HomePanel.java
@@ -1,5 +1,24 @@
 package com.potsko.view;
 
-public class HomePanel {
+import java.awt.BorderLayout;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class HomePanel extends JPanel{
+
+    public HomePanel(MainFrame mainFrame) {
+
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Home Panel");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> mainFrame.showSignup());
+        add(back, BorderLayout.SOUTH);
+        
+    }
 }
