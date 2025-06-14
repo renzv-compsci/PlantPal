81c65eb0c88ef2e27c946f152cea61044f29a5a8
renzv-compsci <viloriarenz917@gmail.com>
2025-06-14 18:37:48 +0800

    Add constants values for the paint component

---
+++ PotsKo/src/main/java/com/potsko/utils/PotsKoConstants.java
@@ -0,0 +1,17 @@
+package com.potsko.utils;
+
+import java.awt.Color;
+
+public class PotsKoConstants {
+
+    // Initializations in utils to keep the code maintanable and reusable
+    public static final int SIDE_NAV_WIDTH = 290;
+    public static final int RIGHT_SIDE_BAR_WIDTH = 420;
+
+    public static final Color COLOR_SIDE_NAV_BG = new Color(174, 192, 148); 
+    public static final Color COLOR_MAIN_CONTENT_BG = Color.WHITE;
+    public static final Color COLOR_AI_BG = new Color(81, 103, 78);
+    public static final Color COLOR_WEATHER_BG = new Color(183, 203, 173);
+
+    public static final Color COLOR_ACTIVE_NAV_BG = new Color (81, 103, 78);
+}
